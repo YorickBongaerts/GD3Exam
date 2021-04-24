1a7d8f051d097c9257cd3a3b403f0858ad807c88
Yorick Bongaerts <yorickbongaerts@telenet.be>
2021-04-24 16:26:18 +0200

    begin of project
    setup except assembly def

---
+++ Assets/GameDesign/Core/AssetBundle/Editor/BundleBuilder.cs
@@ -0,0 +1,108 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using System.IO;
+
+using GameDesign.Core.AssetBundle;
+
+public class BundleBuilder : Editor
+{
+    static string _pathAssembly;
+    static GameInfoData _gameInfoData;
+
+    [MenuItem("Game Design/Export/Build AssetBundles")]
+    static void BuildAssetbundles()
+    {
+        if ( SetAssembly())
+        {
+            SetLayerMatrix();
+            AssetDatabase.SaveAssets();
+            string path = EditorUtility.OpenFolderPanel("Where to save?", "", "");
+            if (!string.IsNullOrEmpty(path))//In case they canceled the action
+            {
+                BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+                Debug.Log("Export was succesfull.");
+            }
+            ClearCache();
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Error", "No assembly found", "ok");
+        }
+     }
+
+    static void SetLayerMatrix()
+    {
+        if (_gameInfoData)
+        {
+            uint maxLayers = 32;
+
+            LayerCollision lc = new LayerCollision();
+            lc.LayerMatrix = new bool[maxLayers * maxLayers];
+            for (int x = 0; x < maxLayers; ++x)
+            {
+                for (int y = 0; y < maxLayers - x; ++y)
+                {
+                    lc.LayerMatrix[(x * maxLayers) + y] = !Physics.GetIgnoreLayerCollision(x, y);
+                }
+            }
+
+            _gameInfoData.LayerInfo = JsonUtility.ToJson(lc);
+        }
+    }
+
+    static bool SetAssembly()
+    {
+        _gameInfoData = null;
+        string[] assets = AssetDatabase.FindAssets("t:GameInfoData");
+        if (assets != null && assets.Length > 0)
+        {
+            GameInfoData info = AssetDatabase.LoadMainAssetAtPath(AssetDatabase.GUIDToAssetPath(assets[0])) as GameInfoData;
+            if (info)
+            {
+                _gameInfoData = info;
+                string assemblyDef = _gameInfoData.AssemblyDefinition;
+                DirectoryInfo dirInfo = new DirectoryInfo(Application.dataPath);
+                dirInfo = new DirectoryInfo(Path.Combine(dirInfo.Parent.ToString(), "Library\\ScriptAssemblies"));
+                string path = dirInfo + "\\" + assemblyDef + ".dll";
+
+                if (File.Exists(path))//Is there an assembly?
+                {
+                    string nameAssembly = "assembly.bytes";
+                    _pathAssembly = Path.Combine(Application.dataPath, nameAssembly);
+
+                    ClearCache();//In case the file already exist
+
+                    File.Copy(path, _pathAssembly);
+                    AssetDatabase.Refresh();
+                    _gameInfoData.AssemblyTextAsset = (TextAsset)AssetDatabase.LoadAssetAtPath("Assets/"+nameAssembly, typeof(TextAsset));
+                    if(_gameInfoData.AssemblyTextAsset != null)
+                    {
+                        return true;
+                    }
+                }
+                Debug.LogWarning("Assembly does not exist, or wrong name...");
+            }
+            Debug.LogWarning("Couldn't find you 'Game Info Data'");
+        }
+
+        return false;
+    }
+
+    static void ClearCache()
+    {
+        if (!string.IsNullOrEmpty(_pathAssembly))
+        {
+            if (File.Exists(_pathAssembly + ".meta"))
+            {
+                File.Delete(_pathAssembly + ".meta");
+            }
+            if (File.Exists(_pathAssembly))
+            {
+                File.Delete(_pathAssembly);
+            }
+
+            AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ Assets/GameDesign/Core/AssetBundle/GameInfoData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace GameDesign.Core.AssetBundle
+{
+    [CreateAssetMenu(fileName = "Game Info Data", menuName = "GameDesign/AssetBundle/Game Info", order = 1)]
+    public class GameInfoData : ScriptableObject
+    {
+        [Header("Information for in the Menu")]
+        [Tooltip("Name of the game.")]
+        public string NameGame = "Game's Name";
+        [TextArea(3, 20)]
+        public string Info = "High Concept / Pitch";
+        public string Genre = "Genre";
+        public string Rating = "Pegi 3";
+        public string NameDeveloper = "Voornaam Achternaam";
+
+        [Header("Visuals")]
+        [Tooltip("Size: 3840px - 1646px, RGB, MipMaps == true, Texture Type: Sprite")]
+        public Sprite BackgroundImage;
+        public Color ThemeColor;
+
+        //[Space]
+        [Header("Project")]
+        public string AssemblyDefinition = "GameDesign.Game1";
+        [Tooltip("Name of the scene.")]
+        public string NameScene = "SampleScene";
+        [HideInInspector]
+        public string NameAssetBundleGame = "";
+        [HideInInspector]
+        public TextAsset AssemblyTextAsset;
+
+        [HideInInspector]
+        public string LayerInfo;
+    }
+
+    [System.Serializable]
+    public class LayerCollision
+    {
+        public bool[] LayerMatrix;
+    }
+}
