e0198f2155fc05058ca9e0a3bbbfb258480af369
Yorick Bongaerts <yorickbongaerts@telenet.be>
2021-05-01 18:24:18 +0200

    made powerupbase
    shorter than expected

--- Assets/Scripts/PowerUpBase.cs
+++ Assets/Scripts/PowerUpBase.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PowerUpBase : MonoBehaviour
 {
     [SerializeField] private MaterialsManager matManager;
+    private void ApplyExtraPower()
+    {
 
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 8)
         {
             matManager.SwapToMaterial = this.gameObject.GetComponent<MeshRenderer>().material;
             matManager.ShouldSwap = true;
+            ApplyExtraPower();
             Destroy(this.gameObject);
         }
     }
